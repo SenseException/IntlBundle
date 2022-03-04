7a9648e6b46b36a61a701070321a5ddcf5065f52
Claudio Zizza <claudio@budgegeria.de>
2022-03-04 23:28:27 +0100

    Move integration test container creation into a trait

--- tests/Integration/FormatterTest.php
+++ tests/Integration/FormatterTest.php
@@ -4,19 +4,17 @@ declare(strict_types=1);
 
 namespace Budgegeria\Bundle\IntlBundle\Tests\Integration;
 
-use Budgegeria\Bundle\IntlBundle\BudgegeriaIntlBundle;
-use Budgegeria\Bundle\IntlBundle\DependencyInjection\BudgegeriaIntlExtension;
 use Budgegeria\Bundle\IntlBundle\Twig\IntlFormatterExtension;
 use Budgegeria\IntlFormat\IntlFormat;
 use Budgegeria\IntlFormat\IntlFormatInterface;
 use DateTime;
 use DateTimeZone;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 
-class FunctionalTest extends TestCase
+class FormatterTest extends TestCase
 {
+    use ContainerTrait;
+
     /**
      * @test
      */
@@ -53,37 +51,5 @@ class FunctionalTest extends TestCase
         $intlFormatterExtension = $container->get(IntlFormatterExtension::class);
 
         self::assertInstanceOf(IntlFormatterExtension::class, $intlFormatterExtension);
-    }
-
-    private function createContainer() : ContainerBuilder
-    {
-        $mappings = [
-            'BudgegeriaIntlBundle' => BudgegeriaIntlBundle::class,
-        ];
-
-        $containerBuilder = new ContainerBuilder(new ParameterBag([
-            'kernel.debug'       => false,
-            'kernel.bundles'     => $mappings,
-            'kernel.environment' => 'test',
-            'kernel.root_dir'    => __DIR__.'/../',
-            'kernel.name'        => 'test',
-            'kernel.cache_dir'   => sys_get_temp_dir(),
-        ]));
-
-        $extension = new BudgegeriaIntlExtension();
-        $containerBuilder->registerExtension($extension);
-        $extension->load([
-            'budgegeria_intl' => [
-                'locale' => 'en_US',
-                'currency' => 'USD'
-            ]
-        ], $containerBuilder);
-
-        $containerBuilder->getDefinition(IntlFormat::class)->setPublic(true);
-        $containerBuilder->getDefinition(IntlFormatterExtension::class)->setPublic(true);
-
-        $containerBuilder->compile();
-
-        return $containerBuilder;
     }
 }
--- tests/Integration/SorterTest.php
+++ tests/Integration/SorterTest.php
@@ -4,15 +4,16 @@ declare(strict_types=1);
 
 namespace Budgegeria\Bundle\IntlBundle\Tests\Integration;
 
-use Budgegeria\Bundle\IntlBundle\BudgegeriaIntlBundle;
-use Budgegeria\Bundle\IntlBundle\DependencyInjection\BudgegeriaIntlExtension;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 
 class SorterTest extends TestCase
 {
-    public function testDefaultSorterBuilderService(): void
+    use ContainerTrait;
+
+    /**
+     * @test
+     */
+    public function defaultSorterBuilderService(): void
     {
         /** @var \Budgegeria\IntlSort\Builder $builder */
         $builder = $this->createContainer()->get('budgegeria_intl_bundle.sorter.builder.standard');
@@ -24,36 +25,5 @@ class SorterTest extends TestCase
         ];
 
         self::assertSame($expected, $sorted);
-    }
-
-    private function createContainer() : ContainerBuilder
-    {
-        $mappings = [
-            'BudgegeriaIntlBundle' => BudgegeriaIntlBundle::class,
-        ];
-
-        $containerBuilder = new ContainerBuilder(new ParameterBag([
-            'kernel.debug'       => false,
-            'kernel.bundles'     => $mappings,
-            'kernel.environment' => 'test',
-            'kernel.root_dir'    => __DIR__.'/../',
-            'kernel.name'        => 'test',
-            'kernel.cache_dir'   => sys_get_temp_dir(),
-        ]));
-
-        $extension = new BudgegeriaIntlExtension();
-        $containerBuilder->registerExtension($extension);
-        $extension->load([
-            'budgegeria_intl' => [
-                'locale' => 'en_US',
-                'currency' => 'USD'
-            ]
-        ], $containerBuilder);
-
-        $containerBuilder->getDefinition('budgegeria_intl_bundle.sorter.builder.standard')->setPublic(true);
-
-        $containerBuilder->compile();
-
-        return $containerBuilder;
     }
 }
