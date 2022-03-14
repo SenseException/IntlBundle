246bcbcacb556511bd927ac844ac94a303428fd0
Claudio Zizza <claudio@budgegeria.de>
2022-03-14 23:58:29 +0100

    Build config for sorter

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Budgegeria\Bundle\IntlBundle\DependencyInjection;
 
+use Budgegeria\Bundle\IntlBundle\DependencyInjection\CompilerPass\SorterBuilderKeyIterator;
+use Budgegeria\IntlSort\Builder;
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -16,13 +19,39 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder('budgegeria_intl');
 
-        $treeBuilder->getRootNode()
+        $rootNode = $treeBuilder->getRootNode();
+        $this->buildGeneric($rootNode);
+        $this->buildSorter($rootNode);
+
+        return $treeBuilder;
+    }
+
+    private function buildGeneric(NodeDefinition $node): void
+    {
+        $node
             ->children()
                 ->scalarNode('locale')->defaultValue('en_US')->end()
                 ->scalarNode('currency')->defaultValue('USD')->end()
             ->end()
         ;
+    }
 
-        return $treeBuilder;
+    private function buildSorter(NodeDefinition $node): void
+    {
+        $sorterChildren = $node->children()
+            ->arrayNode('sorter')
+            ->arrayPrototype()
+            ->children();
+
+        foreach (new SorterBuilderKeyIterator(Builder::class) as $sorterConfigNames) {
+            $sorterChildren->scalarNode($sorterConfigNames)->end();
+        }
+
+        $sorterChildren->scalarNode('locale')->end();
+
+        $sorterChildren->end()
+            ->end()
+            ->end()
+            ->end();
     }
 }
