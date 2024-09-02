515976ba4f28501409a0a444f45c128380a0ac77
Claudio Zizza <claudio@budgegeria.de>
2024-09-02 21:34:10 +0200

    Change Extension class in inheritance for bundle extension
    The extension class of HttpKernel component became internal

--- src/DependencyInjection/BudgegeriaIntlExtension.php
+++ src/DependencyInjection/BudgegeriaIntlExtension.php
@@ -6,8 +6,8 @@ namespace Budgegeria\Bundle\IntlBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 use function dirname;
 
